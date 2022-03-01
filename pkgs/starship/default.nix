0f4c6060aa4bb62a06ff67a64855ab5d06ca8ad2
Jamie Quigley <jamie@quigley.xyz>
2022-03-01 12:58:55 +0000

    Override starship rather than re-creating

--- pkgs/starship/default.nix
+++ pkgs/starship/default.nix
@@ -1,46 +1,19 @@
-{ pkgs ? import <nixpkgs> { }
-, stdenv
-}:
-with pkgs;
+{ starship, fetchFromGitHub, lib }:
 let
-  rev = "78fda3012c68ec807f7e913548061a9b81fd2853";
+  rev = "07870a45877ee3f25471a1a0f100d1312a4f6e3c";
 in
-rustPlatform.buildRustPackage rec {
-  pname = "starship";
+starship.overrideAttrs (old: rec {
   version = "1.3.0-sciencentistguy";
-
   src = fetchFromGitHub {
     owner = "sciencentistguy";
-    repo = pname;
+    repo = "starship";
     inherit rev;
     sha256 = "sha256-obZcwpn3zKBR4ND5Q3GWmOrfbOopr3pQQVRELr7o2xQ=";
   };
 
-  cargoSha256 = "sha256-lkkhUlkzp3nDPyyydU33/JjOgnRXeUWwoMQ/sGW4lvQ=";
-
-  # cargoLock = {
-    # lockFile = builtins.fetchurl "https://raw.githubusercontent.com/Sciencentistguy/starship/${rev}/Cargo.lock";
-  # };
-
-  nativeBuildInputs = [ installShellFiles ] ++ lib.optionals stdenv.isLinux [ pkg-config ];
-
-  buildInputs = lib.optionals stdenv.isLinux [ openssl ]
-    ++ lib.optionals stdenv.isDarwin [ libiconv ];
-
-  postInstall = ''
-    for shell in bash fish zsh; do
-      STARSHIP_CACHE=$TMPDIR $out/bin/starship completions $shell > starship.$shell
-      installShellCompletion starship.$shell
-    done
-  '';
-
-  preCheck = ''
-    HOME=$TMPDIR
-  '';
-
-  # meta = with pkgs.lib; {
-  # description = "A minimal, blazing fast, and extremely customizable prompt for any shell";
-  # homepage = "https://starship.rs";
-  # license = licenses.isc;
-  # };
-}
+  cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+    name = "${old.pname}-${version}-vendor.tar.gz";
+    inherit src;
+    outputHash = "sha256-lkkhUlkzp3nDPyyydU33/JjOgnRXeUWwoMQ/sGW4lvQ=";
+  });
+})
